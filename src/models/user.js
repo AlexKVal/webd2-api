64a232529367dd3edf4099579ea356845249443a
AlexKVal <alexkval@gmail.com>
2016-02-29 23:35:49 +0200

    users.get(id) realization

--- src/models/user.js
+++ src/models/user.js
@@ -5,15 +5,26 @@ const database = getDatabase(`DSN=${process.env.D2ALIAS}`)
 
 const attributes = ['Name', 'Password']
 
-const sql = `SELECT PersID as id,
+const sqlAll = `SELECT PersID as id,
 ${attributes.join(',')}
 FROM sPersonal
 WHERE Hide=False`
 
+const sqlOne = `SELECT PersID as id,
+${attributes.join(',')}
+FROM sPersonal
+WHERE Hide=False AND PersID=?`
+
 const User = {
   all () {
     return database
-      .select(sql)
+      .select(sqlAll)
+      .then((rows) => getSerializer('users', attributes)(rows))
+  },
+
+  get (id) {
+    return database
+      .select(sqlOne, [id])
       .then((rows) => getSerializer('users', attributes)(rows))
   }
 }
