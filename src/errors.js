d4ee98138d0ab839737fca25dc633ac8f84e4e18
AlexKVal <alexkval@gmail.com>
2016-02-29 22:39:26 +0200

    add NotFoundError

--- src/errors.js
+++ src/errors.js
@@ -4,10 +4,19 @@ function DbError (message) {
   this.code = 500
   this.errorType = this.name
 }
-
 DbError.prototype = Object.create(Error.prototype)
 DbError.prototype.name = 'DatabaseError'
 
+function NotFoundError (message) {
+  this.message = message
+  this.stack = new Error().stack
+  this.code = 404
+  this.errorType = this.name
+}
+DbError.prototype = Object.create(Error.prototype)
+DbError.prototype.name = 'NotFoundError'
+
 export {
-  DbError
+  DbError,
+  NotFoundError
 }
