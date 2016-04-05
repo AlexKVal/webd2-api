e676acd07f3f2e8bab73633dd81b112ab74e9cef
AlexKVal <alexkval@gmail.com>
2016-04-05 20:29:45 +0300

    Schema.dataFields contain only data fields descriptions

--- tests/sql-builder/schema-test.js
+++ tests/sql-builder/schema-test.js
@@ -8,3 +8,18 @@ test('Schema: allows "new Schema()" and just "Schema()"', (t) => {
   t.ok(((new Schema({})) instanceof Schema))
   t.end()
 })
+
+test('Schema.dataFields describes only dataFields', (t) => {
+  const schema = new Schema({
+    name: 'string',
+    hide: 'boolean',
+    group: {
+      belongsTo: 'user-group'
+    }
+  })
+
+  t.equal(Object.keys(schema.dataFields).length, 2)
+  t.equal(schema.dataFields.name, 'string')
+  t.equal(schema.dataFields.hide, 'boolean')
+  t.end()
+})
