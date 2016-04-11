b699245d483c7ebdd8e07b277c7fe5b5b156f070
AlexKVal <alexkval@gmail.com>
2016-04-11 11:47:48 +0300

    reduce requirements for Model Schema

--- tests/models/base-model-test.js
+++ tests/models/base-model-test.js
@@ -2,7 +2,6 @@
 const test = require('tape')
 
 const BaseModel = require('../../lib/models/base-model')
-const Schema = require('../../lib/sql-builder/Schema')
 
 const dbMock = {
   exec () {
@@ -45,25 +44,21 @@ test('BaseModel if schema is not provided, then default one is created', functio
   const model = new SomeModel(dbMock, 'some-name')
 
   t.equal(model.name, 'some-name')
-  t.ok((model.schema instanceof Schema))
+  t.ok((model.schema instanceof Object))
   t.end()
 })
 
-test('BaseModel throws if "schema" is not a conf obj or instance of Schema', function (t) {
+test('BaseModel throws if "schema" is not an object', function (t) {
   const fn0 = function fn0 () {
     return new SomeModel(dbMock, 'name', 'any non object')
   }
-  t.throws(fn0, /schema attribute should be an instance of Schema/, 'throws with not an object')
+  t.throws(fn0, /schema attribute should be an object/, 'throws with not an object')
 
   const fn1 = function fn1 () {
     return new SomeModel(dbMock, 'name', {})
   }
-  t.doesNotThrow(fn1, /schema attribute should be an instance of Schema/, 'does not throw with object')
+  t.doesNotThrow(fn1, /schema attribute should be an object/, 'does not throw with object')
 
-  const fn2 = function fn2 () {
-    return new SomeModel(dbMock, 'name', new Schema({}))
-  }
-  t.doesNotThrow(fn2, /schema attribute should be an instance of Schema/, 'does not throw with Schema')
   t.end()
 })
 
