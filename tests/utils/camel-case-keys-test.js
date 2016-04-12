fd4c197797b3638ffa88ee559b00335c41102ff4
AlexKVal <alexkval@gmail.com>
2016-04-12 17:36:15 +0300

    add camelCaseKeys

---
+++ tests/utils/camel-case-keys-test.js
@@ -0,0 +1,23 @@
+'use strict'
+const test = require('tape')
+
+const camelCaseKeys = require('../../lib/utils/camel-case-keys')
+
+test('camelizes all dasherized keys', (t) => {
+  const data = {
+    id: '45',
+    name: 'Admin',
+    'user-group': '1',
+    'user-rights': '3'
+  }
+
+  const normalized = camelCaseKeys(data)
+
+  t.equal(normalized.id, '45')
+  t.equal(normalized.name, 'Admin')
+  t.equal(normalized.userGroup, '1')
+  t.equal(normalized.userRights, '3')
+  t.notok(normalized['user-group'])
+  t.notok(normalized['user-rights'])
+  t.end()
+})
