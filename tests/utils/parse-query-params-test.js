f46b7019768c60df8a0b38fe4bd2caca1a49e245
AlexKVal <alexkval@gmail.com>
2016-05-09 22:19:43 +0300

    parseQueryParams() "related"

--- tests/utils/parse-query-params-test.js
+++ tests/utils/parse-query-params-test.js
@@ -4,16 +4,22 @@ const test = require('tape')
 
 const parseQueryParams = require('../../lib/utils/parse-query-params')
 
-test('parseQueryParams', (t) => {
+test('parseQueryParams()', (t) => {
   const query = {
     related: true,
     fields: ['id', 'name', 'group'],
     filter: { hide: false },
-    order: 'name'
+    order: 'name',
+
+    someUnknownOption: 'value' // it will be ignored
   }
 
+  const parsedOptions = parseQueryParams(query)
+
+  t.equal(parsedOptions.someUnknownOption, undefined, 'unknown option has been ignored')
+
   t.deepEqual(
-    parseQueryParams(query),
+    parsedOptions,
     {
       withRelated: true,
       fieldsOnly: ['id', 'name', 'group'],
@@ -22,5 +28,19 @@ test('parseQueryParams', (t) => {
     },
     'uses only known options'
   )
+
+  function validateParsing (incoming, expected) {
+    t.deepEqual(
+      parseQueryParams(incoming),
+      expected,
+      `${incoming} parses to ${expected}`
+    )
+  }
+
+  validateParsing({related: 'any text value'}, {withRelated: true})
+  validateParsing({/* related is undefined */}, {withRelated: false})
+  validateParsing({related: true}, {withRelated: true})
+  validateParsing({related: false}, {withRelated: false})
+
   t.end()
 })
