d2284200882bb53287d22bf9cbb2a983813b2ca9
AlexKVal <alexkval@gmail.com>
2016-05-14 14:06:34 +0300

    return back old method of model registering
    * adjust userGroup.validateBeforeUpdate() testing

--- tests/models/user-group-test.js
+++ tests/models/user-group-test.js
@@ -2,51 +2,45 @@
 const test = require('tape')
 
 // prevent require('odbc')
-GLOBAL.dbInstance = {}
+GLOBAL.dbInstance = { exec () { return Promise.resolve([]) } }
 
-const { UserGroup } = require('../../lib/models/user-group')
+const registry = require('../../lib/models/registry')
 
 test('UserGroup.validateBeforeUpdate()', (t) => {
-  t.plan(5)
+  t.plan(6)
 
-  const db = { exec () { return Promise.resolve([]) } }
+  registry.reset()
+  const userGroup = require('../../lib/models/user-group')
+  const userModel = require('../../lib/models/user')
 
-  const userModelMock = {
-    selectMany (options) {
-      t.pass('uses user.selectMany()')
-      t.deepEqual(
-        options,
-        { fieldsOnly: 'id', where: { GrpID: 33, hide: false } },
-        'options for user.selectMany()'
-      )
+  t.ok(userGroup, 'userGroup registered')
+  t.ok(userModel, 'user registered')
 
-      return Promise.resolve([ // mock 2 users in the group
-        {id: '123', name: 'user1'},
-        {id: '124', name: 'user2'}
-      ])
-    }
+  userModel.selectMany = (options) => {
+    t.pass('uses user.selectMany()')
+    t.deepEqual(
+      options,
+      { fieldsOnly: 'id', where: { GrpID: 33, hide: false } },
+      'options for user.selectMany()'
+    )
+
+    return Promise.resolve([ // mock 2 users in the group
+      {id: '123', name: 'user1'},
+      {id: '124', name: 'user2'}
+    ])
   }
-
-  const registryMock = {
-    model (modelName) {
-      t.equal(modelName, 'user', 'uses User model')
-      return userModelMock
-    }
-  }
-
-  const userGroup = new UserGroup({db, name: 'userGroup', schema: UserGroup.schemaObject})
 
   const newData = { id: 33, hide: false }
   const prevData = { id: 33, hide: false }
 
   Promise.resolve() // could return just 'true' or Promise
-  .then(() => userGroup.validateBeforeUpdate(123, newData, prevData, registryMock))
+  .then(() => userGroup.validateBeforeUpdate(123, newData, prevData))
   .then((result) => t.ok(result, 'w/o a try to delete should validate OK'))
   .catch((e) => t.fail(e))
 
   .then(() => {
     newData.hide = true // an attempt to delete the group
-    return userGroup.validateBeforeUpdate(123, newData, prevData, registryMock)
+    return userGroup.validateBeforeUpdate(123, newData, prevData)
   })
   .then(() => t.fail('should not be OK'))
   .catch((e) => t.equal(
